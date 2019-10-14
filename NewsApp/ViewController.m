8f962ea682d14329775ab9396a0cf93beea89346
happy-yuxuan <victor.wuy@icloud.com>
2019-10-14 08:28:54 +0800

    04 | 使用Xcode创建第一个工程
    添加.gitignore文件
    在屏幕居中显示一个 "hello world"
    
    总结：
    1. 可以使用括号的C的复合语句让代码更加简洁，详细参考文档：https://gcc.gnu.org/onlinedocs/gcc/Statement-Exprs.html

--- NewsApp/ViewController.m
+++ NewsApp/ViewController.m
@@ -17,6 +17,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self.view addSubview:({
+        UILabel *label = [[UILabel alloc] init];
+        label.text = @"hello world";
+        [label sizeToFit];
+        label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2);
+        label;
+    })];
 }
 
 
