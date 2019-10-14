ab497e60732def3a7a41b158227c2396aac6f116
happy-yuxuan <victor.wuy@icloud.com>
2019-10-14 08:46:26 +0800

    06 | iOS中的视图UIView
    删除原来Label。 在屏幕上添加两个View
    
    总结：
    UIView为最基础的类，管理渲染屏幕区域的内容展示。
    
    作为各种View类型的父类，有基础属性(frame)和能力（addSubView etc.）。
    
    bounds表示相对于自己坐标系的位置和大小，frame表示相对于父坐标系的位置和大小。
    
    使用栈管理全部子View，栈顶的先显示。addSubView就是压一个View到SubViews Stack中。

--- NewsApp/ViewController.m
+++ NewsApp/ViewController.m
@@ -17,13 +17,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    [self.view addSubview:({
-        UILabel *label = [[UILabel alloc] init];
-        label.text = @"hello world";
-        [label sizeToFit];
-        label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2);
-        label;
-    })];
+    
+    UIView *view = [[UIView alloc] init];
+    view.backgroundColor = [UIColor redColor];
+    view.frame = CGRectMake(100, 100, 100, 100); // frame是相对于父视图的位置
+    
+    UIView *view2 = [[UIView alloc] init];
+    view2.backgroundColor = [UIColor greenColor];
+    view2.frame = CGRectMake(150, 150, 100, 100);
+    
+    // view2 在 view的上面，因为是栈结构。 逐渐压栈，显示的是栈顶的View
+    [self.view addSubview:view];
+    [self.view addSubview:view2];
 }
 
 
