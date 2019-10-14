fed23920aaf1c51a9d893182f79a936419cbe7b9
happy-yuxuan <victor.wuy@icloud.com>
2019-10-14 15:11:06 +0800

    09 | 结合视图和控制器构建TabBar样式界面
    1. 添加项目用到的图片资源
    2. 在App开始时候添加一个TabBarController , 之中有4个Controller对应新闻，视频，推荐，我的 （因为是iOS13，并是像在视频中那样添加在appdelegate中）
    
    PS：在评论区，iOS13之后，appdelegate的指责发生了变化，具体的可以参考https://developer.apple.com/videos/play/wwdc2019/42 这个session。 简单的说和UI生命周期相关的回调都交给了SceneDelegate 这个delegate，而appdelegate主要负责 app 生命周期相关的回调。对于window来说，作为一个特殊的UI，也自然的变成了SceneDelegate的一个property，我们可以在SceneDelegate的头文件中找到它，并在willConnectToSession这个函数中使用。简单的说，在iOS13中将对应的代码移动到SceneDelegate.m中即可
    
    总结：
    生命周期 流程： init(创建Controller) → viewDidLoad(里面所有的View在内存加载完毕，还没显示，所以很多初试化在此操作) → viewWillAppear:(开始加载到屏幕的瞬间) → viewDidAppear:（加载完成的瞬间） → viewWillDisappear:（即将离开屏幕的瞬间） → viewDidDisappear:（完全离开屏幕的瞬间） → Dealloc（销毁的时候）

--- NewsApp/SceneDelegate.m
+++ NewsApp/SceneDelegate.m
@@ -6,13 +6,43 @@
 
 @implementation SceneDelegate
 
-
 - (void)scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions {
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-}
 
+    self.window = [[UIWindow alloc] initWithWindowScene:(UIWindowScene *)scene];
+    UITabBarController *tabbarController = [[UITabBarController alloc] init];
+
+    UIViewController *controller1 = [[UIViewController alloc] init];
+    controller1.view.backgroundColor = [UIColor redColor];
+    controller1.tabBarItem.title = @"新闻";
+    controller1.tabBarItem.image = [UIImage imageNamed:@"page"];
+    controller1.tabBarItem.selectedImage = [UIImage imageNamed:@"page_selected"];
+
+    UIViewController *controller2 = [[UIViewController alloc] init];
+    controller2.view.backgroundColor = [UIColor yellowColor];
+    controller2.tabBarItem.title = @"视频";
+    controller2.tabBarItem.image = [UIImage imageNamed:@"video"];
+    controller2.tabBarItem.selectedImage = [UIImage imageNamed:@"video_selected"];
+
+    UIViewController *controller3 = [[UIViewController alloc] init];
+    controller3.view.backgroundColor = [UIColor greenColor];
+    controller3.tabBarItem.title = @"推荐";
+    controller3.tabBarItem.image = [UIImage imageNamed:@"like"];
+    controller3.tabBarItem.selectedImage = [UIImage imageNamed:@"like_selected"];
+
+    UIViewController *controller4 = [[UIViewController alloc] init];
+    controller4.view.backgroundColor = [UIColor lightGrayColor];
+    controller4.tabBarItem.title = @"我的";
+    controller4.tabBarItem.image = [UIImage imageNamed:@"home"];
+    controller4.tabBarItem.selectedImage = [UIImage imageNamed:@"home_selected"];
+
+    [tabbarController setViewControllers:@[controller1, controller2, controller3, controller4]];
+
+    self.window.rootViewController = tabbarController;
+    [self.window makeKeyAndVisible];
+}
 
 - (void)sceneDidDisconnect:(UIScene *)scene {
     // Called as the scene is being released by the system.
@@ -21,30 +51,25 @@
     // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
 }
 
-
 - (void)sceneDidBecomeActive:(UIScene *)scene {
     // Called when the scene has moved from an inactive state to an active state.
     // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
 }
-
 
 - (void)sceneWillResignActive:(UIScene *)scene {
     // Called when the scene will move from an active state to an inactive state.
     // This may occur due to temporary interruptions (ex. an incoming phone call).
 }
 
-
 - (void)sceneWillEnterForeground:(UIScene *)scene {
     // Called as the scene transitions from the background to the foreground.
     // Use this method to undo the changes made on entering the background.
 }
-
 
 - (void)sceneDidEnterBackground:(UIScene *)scene {
     // Called as the scene transitions from the foreground to the background.
     // Use this method to save data, release shared resources, and store enough scene-specific state information
     // to restore the scene back to its current state.
 }
-
 
 @end
