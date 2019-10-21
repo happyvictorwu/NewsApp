fa414b69028a6c5f9a11a8bb0cc1d24ffd5ba142
happy-yuxuan <victor.wuy@icloud.com>
2019-10-21 16:28:33 +0800

    21 | 应用UIScrollViewDelegate实现复杂逻辑
    实现UIScrollViewDelegate的代理方法，scrollViewDidScroll:在用户在手势操作的途中、 scrollViewWillBeginDragging - 用户手势开始的时候、 scrollViewDidEndDragging - 用户手势松手的时候、scrollViewWillBeginDecelerating - 用户手势开始减速的时候、 scrollViewDidEndDecelerating - 用户手势减速结束的时候

--- NewsApp/YXRecommendViewController.m
+++ NewsApp/YXRecommendViewController.m
@@ -8,7 +8,7 @@
 
 #import "YXRecommendViewController.h"
 
-@interface YXRecommendViewController ()
+@interface YXRecommendViewController ()<UIScrollViewDelegate>
 
 @end
 
@@ -28,12 +28,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor greenColor];
-    
+
     UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
     scrollView.backgroundColor = [UIColor lightGrayColor];
     scrollView.contentSize = CGSizeMake(self.view.bounds.size.width * 5, self.view.bounds.size.height);
     scrollView.showsHorizontalScrollIndicator = NO;   // scrollView属性：设置水平滚动条不显示
-    
+    scrollView.delegate = self;
+
     NSArray *colorArray = @[[UIColor redColor], [UIColor blueColor], [UIColor yellowColor], [UIColor lightGrayColor], [UIColor grayColor]];
     for (int i = 0; i < 5; i++) {
         [scrollView addSubview:({
@@ -43,7 +44,7 @@
         })];
     }
     scrollView.pagingEnabled = YES;   // scrollView属性：滑动超过半个屏幕就有翻页效果
-    
+
     [self.view addSubview:scrollView];
 }
 
@@ -56,5 +57,26 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+// MARK: - Delegate
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+//    NSLog(@"scrollViewDidScroll - 用户在手势操作的途中%@", @(scrollView.contentOffset.x));
+}
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
+    NSLog(@"scrollViewWillBeginDragging - 用户手势开始的时候");
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate {
+    NSLog(@"scrollViewDidEndDragging - 用户手势松手的时候");
+}
+
+- (void)scrollViewWillBeginDecelerating:(UIScrollView *)scrollView {
+    NSLog(@"scrollViewWillBeginDecelerating - 用户手势开始减速的时候");
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+    NSLog(@"scrollViewDidEndDecelerating - 用户手势减速结束的时候");
+}
 
 @end
