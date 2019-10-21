595bfdb7348ac5e1bd0d93de090b05320386d50c
happy-yuxuan <victor.wuy@icloud.com>
2019-10-21 11:12:43 +0800

    15 | UITableViewCell的复用及原理
    在tableView:cellForRowAtIndexPath:代理方法中使用复用cell的逻辑(复用回收池)
    
    总结：
    1. cell在滚出屏幕外面的时候会去到系统回收池中。
    2. 使用复用回收池，在用cell的使用看看能不能再复用回收池根据id找到对应的cell，若没找到再创建新的cell，找到了直接复用

--- NewsApp/ViewController.m
+++ NewsApp/ViewController.m
@@ -86,8 +86,13 @@
 
 // cell长成什么样子
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    // 使用系统提供的样式Style  (UITableViewCellStyleSubtitle)
-    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"id"];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"id"];  // 使用复用回收机制，在tableview的回收池中能不能找到这个相同id的cell（相同id的样式是一样的）
+
+    if (!cell) {  // 若没有cell,创建一个新的cell。
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"id"];
+    }
+
+    // 若系统回收池取到了，就用回收的cell展示就行
     // 系统提供样式的属性
     cell.textLabel.text = @"主标题";
     cell.detailTextLabel.text = @"副标题";
