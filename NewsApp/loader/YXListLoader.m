179c9ca3c8ddc1cb72d0a655f31d68129336f2ae
happy-yuxuan <victor.wuy@icloud.com>
2019-10-30 16:11:30 +0800

    43 | iOS中的JSON解析
    放弃AFNetworking，自己通过NSURLSessionDataTask把收到的请求（NSData）转换成 (NSDictionary)
    
    总结：
    1. 系统最基础的库： NSJSONSerialization 提供JSON数据和系统对象之间的转换（可以相互转换）

--- NewsApp/loader/YXListLoader.m
+++ NewsApp/loader/YXListLoader.m
@@ -12,27 +12,30 @@
 @implementation YXListLoader
 
 - (void)loadListData {
-    [[AFHTTPSessionManager manager] GET:@"http://v.juhe.cn/toutiao/index?type=top&key=97ad001bfcc2082e2eeaf798bad3d54e" parameters:nil progress:^(NSProgress *_Nonnull downloadProgress) {
-    } success:^(NSURLSessionDataTask *_Nonnull task, id _Nullable responseObject) {
-        NSLog(@"");
-    } failure:^(NSURLSessionDataTask *_Nullable task, NSError *_Nonnull error) {
+//    [[AFHTTPSessionManager manager] GET:@"http://v.juhe.cn/toutiao/index?type=top&key=97ad001bfcc2082e2eeaf798bad3d54e" parameters:nil progress:^(NSProgress *_Nonnull downloadProgress) {
+//    } success:^(NSURLSessionDataTask *_Nonnull task, id _Nullable responseObject) {
+//        NSLog(@"");
+//    } failure:^(NSURLSessionDataTask *_Nullable task, NSError *_Nonnull error) {
+//        NSLog(@"");
+//    }];
+
+    NSString *urlString = @"http://v.juhe.cn/toutiao/index?type=top&key=97ad001bfcc2082e2eeaf798bad3d54e";
+    NSURL *listURL = [NSURL URLWithString:urlString];
+
+//    __unused NSURLRequest *listRequest = [NSURLRequest requestWithURL:listURL];
+
+    NSURLSession *session = [NSURLSession sharedSession];
+
+    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:listURL completionHandler:^(NSData *_Nullable data, NSURLResponse *_Nullable response, NSError *_Nullable error) {
+        NSError *jsonError;
+        id jsonObj = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];  // change to NSdata to Dictionary
+
         NSLog(@"");
     }];
 
-//    NSString *urlString = @"http://v.juhe.cn/toutiao/index?type=top&key=97ad001bfcc2082e2eeaf798bad3d54e";
-//    NSURL *listURL = [NSURL URLWithString:urlString];
-//
-////    __unused NSURLRequest *listRequest = [NSURLRequest requestWithURL:listURL];
-//
-//    NSURLSession *session = [NSURLSession sharedSession];
-//
-//    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:listURL completionHandler:^(NSData *_Nullable data, NSURLResponse *_Nullable response, NSError *_Nullable error) {
-//        NSLog(@"");
-//    }];
-//
-//    [dataTask resume];
-//
-//    NSLog(@"");
+    [dataTask resume];
+
+    NSLog(@"");
 }
 
 @end
