3f24f6f694d725372f5aabd04aa884ede396a97b
happy-yuxuan <victor.wuy@icloud.com>
2019-11-01 17:05:32 +0800

    47 | 使用NSFileManager和NSFileHandle操作和管理文件
    在系统的Library-Cache文件夹下，创建文件夹名为YXData，再在其中创建一个文件名为list，并对list加入字符串。
    
    总结：
    
    NSFileManager：
    1. fileManager是一个单例。 主要用于创建文件、删除文件、查询文件、移动和复制等等。根据用户给出的路径操作
    2. 创建文件，删除文件，查询文件，移动和复制文件等
    3. 读取文件内容
    4. 通过NSURL或者NSString作为Path进行操作
    
    NSFileHandle：
    1. NSFileHandle是单例。 读/写文件
    2. 常用于追加操作 （在offset移动到文件莫问，然后追下字符）
    3. 记得操作完关闭文件

--- NewsApp/NewsController/Medel/YXListLoader.m
+++ NewsApp/NewsController/Medel/YXListLoader.m
@@ -48,7 +48,44 @@
 }
 
 - (void)_getSandBoxPath {
-    __unused NSArray *pathArray = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSArray *pathArray = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+    NSString *cachePath = [pathArray firstObject];
+
+    // fileManager是一个单例。 用于创建文件、删除文件、查询文件、移动和复制等等。根据用户给出的路径操作
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+
+    // 创建文件夹
+    NSString *dataPath = [cachePath stringByAppendingPathComponent:@"YXData"];
+    NSError *createError;
+    [fileManager createDirectoryAtPath:dataPath withIntermediateDirectories:YES attributes:nil error:&createError];  // 创建一个文件夹根据dataPath
+
+    // 创建文件
+    NSString *listDataPath = [dataPath stringByAppendingPathComponent:@"list"];
+    NSData *listData = [@"abc" dataUsingEncoding:NSUTF8StringEncoding];      // 使用utf8编码字符串，变成一个二进制文件。计算机是以二进制存储的
+    [fileManager createFileAtPath:listDataPath contents:listData attributes:nil];      // 在给定路径创建文件，内容contents为listData(字符abc)
+
+    // 查询文件
+    __unused BOOL fileExist = [fileManager fileExistsAtPath:listDataPath];  // 判断listDataPath是否存在
+
+    // 删除
+//    if (fileExist) {
+//        [fileManager removeItemAtPath:listDataPath error:nil];
+//    }
+
+    NSLog(@"");
+
+    // -------- NSFileHandle
+
+    // 单例。 读/写/截断/刷新/追加数据等等。常用于追加数据。 以下对listDataPath的这个文件创造了一个NSFileHandle对其进行操作
+    NSFileHandle *fileHandler = [NSFileHandle fileHandleForUpdatingAtPath:listDataPath];
+
+    // 对文件数据追加操作
+    [fileHandler seekToEndOfFile];  // 把offset调到文件末尾，因为要在文件末尾追加数据
+    [fileHandler writeData:[@"--append: def" dataUsingEncoding:NSUTF8StringEncoding]];  // 使用utf8编码字符串写在offset后面
+
+    [fileHandler synchronizeFile];  // 刷新文件 对实时要求高就刷新
+    [fileHandler closeFile];  // 关闭文件操作。不关系统结束才关，但是最好每次使用完就关
+
     NSLog(@"");
 }
 
